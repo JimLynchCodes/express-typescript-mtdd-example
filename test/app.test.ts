634b20d72278308fbd380ab980ac7f3785a1d8bc
YOUR_GITHUB_USERNAME <YOUR_GITHUB_EMAIL>
2023-08-02 21:08:06 -0400

    example express ts with jest

--- test/app.test.ts
+++ test/app.test.ts
@@ -2,11 +2,32 @@ import request from 'supertest';
 
 import app from '../src/app';
 
-describe('app', () => {
-  it('responds with a not found message', (done) => {
+describe('not found routes', () => {
+  it('responds with emoji in production', (done) => {
+    
+    process.env['NODE_ENV'] = "production"
+    
     request(app)
       .get('/what-is-this-even')
       .set('Accept', 'application/json')
+      .expect('Content-Type', /json/)
+      .expect(404, done);
+  });
+  it('responds with a not found message when not production', (done) => {
+    
+    process.env['NODE_ENV'] = "not production"
+    
+    request(app)
+      .get('/what-is-this-even')
+      .set('Accept', 'application/json')
+      .expect('Content-Type', /json/)
+      .expect(404, done);
+  });
+  it('', (done) => {
+    
+    request(app)
+      .get('/')
+      .set('Accept', 'text')
       .expect('Content-Type', /json/)
       .expect(404, done);
   });
